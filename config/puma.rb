691464ac04aa41c21c9f123c09f65347768dcc27
MPBunce <matthew-bunce@outlook.com>
2026-04-16 12:09:01 -0400

    test

--- config/puma.rb
+++ config/puma.rb
@@ -1,15 +1,9 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
-if ENV["RAILS_ENV"] == "production"
-  bind "unix:///var/run/puma/my_app.sock"
-else
-  port ENV.fetch("PORT", 3000)
-end
+port ENV.fetch("PORT", 8080)
 
 environment ENV.fetch("RAILS_ENV", "development")
 
 plugin :tmp_restart
-plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
-
 pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
