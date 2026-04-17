bcb27ad2a702a1658b67185d1ccc535acde395ca
MPBunce <matthew-bunce@outlook.com>
2026-04-16 23:45:09 -0400

    bro run

--- config/puma.rb
+++ config/puma.rb
@@ -1,19 +1,12 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
-# Elastic Beanstalk provides PORT
-port ENV.fetch("PORT", 8080)
+bind "unix:///var/run/puma/my_app.sock"
 
 environment ENV.fetch("RAILS_ENV", "development")
 
-# Restart support
-plugin :tmp_restart
-
-# PID file (optional but fine)
 pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
 
-# Optional: preload for performance (safe for most apps)
 preload_app!
 
-# Allow puma to be restarted by `bin/rails restart`
 plugin :tmp_restart
