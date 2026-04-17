d86cff4701e84ae2b840ac2d25d715f71aaa82e2
MPBunce <matthew-bunce@outlook.com>
2026-04-17 17:54:31 -0400

    it builds

--- config/puma.rb
+++ config/puma.rb
@@ -1,12 +1,20 @@
+environment ENV.fetch("RAILS_ENV", "development")
+pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
 threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
-bind "unix:///var/run/puma/my_app.sock"
-
 environment ENV.fetch("RAILS_ENV", "development")
+
+if ENV.fetch("RAILS_ENV", "development") == "production"
+	bind "unix:///var/run/puma/my_app.sock"
+else
+	port ENV.fetch("PORT", 3000)
+end
 
 pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
 
 preload_app!
+
+plugin :tmp_restart
 
 plugin :tmp_restart
