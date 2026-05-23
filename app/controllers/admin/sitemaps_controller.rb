f2bd63638f16b40449a6ed5eab3c4453168d80e8
MPBunce <matthew-bunce@outlook.com>
2026-05-23 18:27:59 -0400

    fix controller

--- app/controllers/admin/sitemaps_controller.rb
+++ app/controllers/admin/sitemaps_controller.rb
@@ -1,4 +1,6 @@
-def rebuild
-  SitemapRebuildJob.perform_now
-  redirect_to admin_settings_path, notice: "Sitemap rebuilt."
+class Admin::SitemapsController < Admin::BaseController
+  def rebuild
+    SitemapRebuildJob.perform_now
+    redirect_to admin_settings_path, notice: "Sitemap rebuilt."
+  end
 end
