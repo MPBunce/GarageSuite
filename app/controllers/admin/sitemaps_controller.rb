38460ee99ad28a1f531ad9ce3276ffeef033f489
MPBunce <matthew-bunce@outlook.com>
2026-05-23 18:17:49 -0400

    jobs

--- app/controllers/admin/sitemaps_controller.rb
+++ app/controllers/admin/sitemaps_controller.rb
@@ -1,6 +1,4 @@
-class Admin::SitemapsController < Admin::BaseController
-  def rebuild
-    SitemapRebuildJob.perform_later
-    redirect_to admin_settings_path, notice: "Sitemap rebuild queued."
-  end
+def rebuild
+  SitemapRebuildJob.perform_now
+  redirect_to admin_settings_path, notice: "Sitemap rebuilt."
 end
--- app/jobs/sitemap_rebuild_job.rb
+++ app/jobs/sitemap_rebuild_job.rb
@@ -1,8 +1,12 @@
-# app/jobs/sitemap_rebuild_job.rb
 class SitemapRebuildJob < ApplicationJob
   queue_as :default
 
   def perform
-    `bundle exec rake sitemap:refresh`
+    SitemapGenerator::Sitemap.default_host = "https://www.#{AppSetting.value('root_url')}"
+    SitemapGenerator::Sitemap.create force: true do
+      add root_path, priority: 1.0, changefreq: 'daily'
+      add booking_path, priority: 0.8, changefreq: 'weekly'
+      add appointment_lookup_path, priority: 0.5, changefreq: 'monthly'
+    end
   end
 end
