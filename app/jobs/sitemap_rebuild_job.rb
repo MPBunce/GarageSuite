5b8840913a7e033d5f45f227effbf6d116662e6d
MPBunce <matthew-bunce@outlook.com>
2026-05-23 18:42:38 -0400

    sitempa

--- app/jobs/sitemap_rebuild_job.rb
+++ app/jobs/sitemap_rebuild_job.rb
@@ -3,10 +3,14 @@ class SitemapRebuildJob < ApplicationJob
 
   def perform
     SitemapGenerator::Sitemap.default_host = "https://www.#{AppSetting.value('root_url')}"
-    SitemapGenerator::Sitemap.create force: true do
+    puts "Building sitemap for: #{SitemapGenerator::Sitemap.default_host}"
+
+    SitemapGenerator::Sitemap.create do
       add root_path, priority: 1.0, changefreq: 'daily'
       add booking_path, priority: 0.8, changefreq: 'weekly'
       add appointment_lookup_path, priority: 0.5, changefreq: 'monthly'
     end
+
+    puts "Sitemap built successfully at #{Time.current}"
   end
 end
